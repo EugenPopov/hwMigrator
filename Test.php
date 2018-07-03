24f2184d734df69ebee40295c0f585aebea9081a
greeflas <vldmr.kuprienko@gmail.com>
2018-07-03 10:12:44 +0300

    Creates basic functionality
    - adds type hints to methods params

---
+++ Test.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+/**
+ * Class for testing of the migration tool.
+ *
+ * @author Vladimir Kuprienko <vldmr.kuprienko@gmail.com>
+ */
+class Test
+{
+    /**
+     * This is first test method.
+     *
+     * @param float     $param1
+     * @param string    $param2
+     * @param null|int  $param3
+     */
+    public function testMethod($param1, $param2, $param3)
+    {
+    }
+
+    /**
+     * This is second test method.
+     *
+     * @param null|object $data
+     */
+    public function test($data)
+    {
+    }
+}
