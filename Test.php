4ed931b4114da5053dba56fea4206512bf82f6ba
Popov Eugene <zhenya1995q@gmail.com>
2018-07-04 16:29:43 +0300

    Added return type hints migration

--- Test.php
+++ Test.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App;
-
 /**
  * Class for testing of the migration tool.
  *
@@ -15,17 +13,22 @@ class Test
      * @param float     $param1
      * @param string    $param2
      * @param null|int  $param3
+     *
+     * @return void|null
      */
-    public function testMethod($param1, $param2, $param3)
+    public function testMethod(float $param1, string $param2, ?int $param3) :?void
     {
-    }
 
+    }
     /**
      * This is second test method.
      *
      * @param null|object $data
+     *
+     * @return void
      */
-    public function test($data)
+    public function test(?object $data) :void
     {
+
     }
 }
